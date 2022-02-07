8ccf3e9604ea5d128d0bdaa58ed41d31132435bf
mbilek333 <19591770+mikeb333@users.noreply.github.com>
2022-02-07 11:50:20 -0700

    Fixed iframe tests.

--- libs/iframe/bestow-iframe.mjs
+++ libs/iframe/bestow-iframe.mjs
@@ -3,66 +3,63 @@
 import styling from 'bundle-text:./styles.css';
 
 const openGutter = url => () => {
-    const frame = document.getElementById('bestow-frame');
-    const gutter = document.getElementById('bestow-gutter');
-    const closeElem = document.getElementById('bestow-close');
+  const frame = document.getElementById('bestow-frame');
+  const gutter = document.getElementById('bestow-gutter');
+  const closeElem = document.getElementById('bestow-close');
 
-    frame.setAttribute('src', url);
-    closeElem.style.display = 'block';
-    gutter.style.display = 'block';
+  frame.setAttribute('src', url);
+  closeElem.style.display = 'block';
+  gutter.style.display = 'block';
 
-    return false;
+  return false;
 };
 
 const closeGutter = () => () => {
-    const gutter = document.getElementById('bestow-gutter');
-    const closeElem = document.getElementById('bestow-close');
+  const gutter = document.getElementById('bestow-gutter');
+  const closeElem = document.getElementById('bestow-close');
 
-    /* Disable slideout visuals */
-    closeElem.style.display = 'none';
-    gutter.style.display = 'none';
+  /* Disable slideout visuals */
+  closeElem.style.display = 'none';
+  gutter.style.display = 'none';
 };
 
 function setupBestow(elementSelector, url) {
-    const hostElem = document.querySelector(elementSelector);
+  const hostElem = document.querySelector(elementSelector);
 
-    if (!hostElem) {
-        console.error(`Couldn't find element to bind to`);
-        return;
-    }
+  if (!hostElem) {
+    throw new Error(`Supplied element could not be found`);
+  }
 
-    hostElem.onclick = openGutter(url);
+  hostElem.onclick = openGutter(url);
 
-    const styleSheet = document.createElement('style');
-    styleSheet.textContent = styling;
-    styleSheet.id = `bestow-styling`;
-    document.body.appendChild(styleSheet);
+  const styleSheet = document.createElement('style');
+  styleSheet.textContent = styling;
+  styleSheet.id = `bestow-styling`;
+  document.body.appendChild(styleSheet);
 
-    const gutterElem = document.createElement('div');
-    gutterElem.id = 'bestow-gutter';
-    gutterElem.classList.add('gutter');
-    gutterElem.style.display = 'none';
+  const gutterElem = document.createElement('div');
+  gutterElem.id = 'bestow-gutter';
+  gutterElem.classList.add('gutter');
+  gutterElem.style.display = 'none';
 
-    const closeSpan = document.createElement('span');
-    closeSpan.textContent = 'X';
-    closeSpan.classList.add('close-span');
+  const closeSpan = document.createElement('span');
+  closeSpan.textContent = 'X';
+  closeSpan.classList.add('close-span');
 
-    const closeElem = document.createElement('div');
-    closeElem.id = 'bestow-close';
-    closeElem.classList.add('close');
-    closeElem.style.display = 'none';
-    closeElem.appendChild(closeSpan);
-    closeElem.onclick = closeGutter();
-    gutterElem.appendChild(closeElem);
+  const closeElem = document.createElement('div');
+  closeElem.id = 'bestow-close';
+  closeElem.classList.add('close');
+  closeElem.style.display = 'none';
+  closeElem.appendChild(closeSpan);
+  closeElem.onclick = closeGutter();
+  gutterElem.appendChild(closeElem);
 
-    const frameElem = document.createElement('iframe');
-    frameElem.id = 'bestow-frame';
-    frameElem.allow = 'payment';
-    frameElem.src = url;
-    gutterElem.appendChild(frameElem);
-    document.body.appendChild(gutterElem);
-
-
+  const frameElem = document.createElement('iframe');
+  frameElem.id = 'bestow-frame';
+  frameElem.allow = 'payment';
+  frameElem.src = url;
+  gutterElem.appendChild(frameElem);
+  document.body.appendChild(gutterElem);
 }
 
 window.Bestow = { setup: setupBestow };
--- libs/iframe/bestow-iframe.test.js
+++ libs/iframe/bestow-iframe.test.js
@@ -1,123 +1,157 @@
 require('./bestow-iframe.mjs');
 
-const describeIf = condition => condition ? describe : describe.skip;
+const describeIf = condition => (condition ? describe : describe.skip);
 
-const testURL = "https://somegarbage.bestow.com";
-const testElementID = "some_id";
+const testURL = 'https://somegarbage.bestow.com';
+const testElementID = 'some_id';
 
-const iframeElementID = "bestow-frame";
-const gutterElementID = "bestow-gutter";
-const closeElementID = "bestow-close";
-const styleElementID = "bestow-styling";
+const iframeElementID = 'bestow-frame';
+const gutterElementID = 'bestow-gutter';
+const closeElementID = 'bestow-close';
+const styleElementID = 'bestow-styling';
 
 document.body.innerHTML += `
-        <a id="${testElementID}" href="#" rel="noreferrer noopener">
-          <span>Get a Quote</span>
-        </a>
-    `;
+    <a id="${testElementID}" href="#" rel="noreferrer noopener">
+      <span>Get a Quote</span>
+    </a>
+`;
 
-window.Bestow.setup(`#${testElementID}`, testURL);
+let setupError;
+try {
+  window.Bestow.setup(`#${testElementID}`, testURL);
+} catch (e) {
+  setupError = e;
+}
 
-console.log(document.documentElement.innerHTML)
+describe('test setup', () => {
+  test('error when supplied element does not exist', () => {
+    let err;
+    try {
+      window.Bestow.setup(`garbage`, testURL);
+    } catch (e) {
+      err = e;
+    }
+    expect(err).toBeTruthy();
+    expect(err.message).toBe('Supplied element could not be found');
+  });
+  test('setup is successful', () => {
+    expect(setupError).toBeFalsy();
+  });
+});
 
+console.log(document.documentElement.innerHTML);
+
+const suppliedTestElement = document.getElementById(testElementID);
 const gutterElement = document.getElementById(gutterElementID);
 const iframeElement = document.getElementById(iframeElementID);
 const closeElement = document.getElementById(closeElementID);
-const styleElement = document.getElementById(styleElementID)
+const styleElement = document.getElementById(styleElementID);
 
-describe("elements exist", () => {
-    test('gutter element exists', () => {
-        expect(gutterElement).toBeTruthy();
-    });
-
-    test('iframe element exists', () => {
-        expect(iframeElement).toBeTruthy();
-    });
-
-    test('close element exists', () => {
-        expect(closeElement).toBeTruthy();
-    });
-
-    test('style element exists', () => {
-        expect(styleElement).toBeTruthy();
-    });
-});
-
+const suppliedExists = suppliedTestElement !== null;
 const gutterExists = gutterElement !== null;
 const iframeExists = iframeElement !== null;
 const closeExists = closeElement !== null;
 const styleExists = styleElement !== null;
 
-describeIf(gutterExists)('gutter tests', () => {
-    afterAll(() => {
-        if (closeExists) {
-            closeElement.click();
-        }
-    });
+describe('elements exist', () => {
+  test('supplied test element exists', () => {
+    expect(suppliedExists).toBeTruthy();
+  });
 
-    test("gutter element has correct id", () => {
-        expect(gutterElement.id).toBe(gutterElementID);
-    });
-    test("gutter element has correct class name", () => {
-        expect(gutterElement.className).toBe('gutter');
-    });
+  test('gutter element exists', () => {
+    expect(gutterExists).toBeTruthy();
+  });
+
+  test('iframe element exists', () => {
+    expect(iframeExists).toBeTruthy();
+  });
+
+  test('close element exists', () => {
+    expect(closeExists).toBeTruthy();
+  });
+
+  test('style element exists', () => {
+    expect(styleExists).toBeTruthy();
+  });
+});
+
+describeIf(gutterExists)('gutter tests', () => {
+  afterAll(() => {
+    if (closeExists) {
+      closeElement.click();
+    }
+  });
+
+  test('gutter element has correct id', () => {
+    expect(gutterElement.id).toBe(gutterElementID);
+  });
+  test('gutter element has correct class name', () => {
+    expect(gutterElement.className).toBe('gutter');
+  });
+  test('gutter element is not displayed on load', () => {
+    expect(gutterElement.style.display).toBe('none');
+  });
+
+  test('gutter element is displayed when supplied element is clicked', () => {
+    suppliedTestElement.click();
+    expect(gutterElement.style.display).toBe('block');
+  });
+  test('gutter element is hidden when closed', () => {
+    closeElement.click();
+    expect(gutterElement.style.display).toBe('none');
+  });
 });
 
 describeIf(iframeExists)('iframe tests', () => {
-    afterAll(() => {
-        if (closeExists) {
-            closeElement.click();
-        }
-    });
+  afterAll(() => {
+    if (closeExists) {
+      closeElement.click();
+    }
+  });
 
-    test("iframe element has correct id", () => {
-        expect(iframeElement.id).toBe(iframeElementID);
-    })
+  test('iframe element has correct id', () => {
+    expect(iframeElement.id).toBe(iframeElementID);
+  });
+  test('iframe element has correct src attribute', () => {
+    const srcAttribute = iframeElement.getAttribute('src');
+    expect(srcAttribute).toBe(testURL);
+  });
 });
 
 describeIf(closeExists)('close tests', () => {
-    afterAll(() => {
-        if (closeExists) {
-            closeElement.click();
-        }
-    });
+  afterAll(() => {
+    if (closeExists) {
+      closeElement.click();
+    }
+  });
 
-    test("close element has correct id", () => {
-        expect(closeElement.id).toBe(closeElementID);
-    })
+  test('close element has correct id', () => {
+    expect(closeElement.id).toBe(closeElementID);
+  });
+  test('close element has correct class name', () => {
+    expect(closeElement.className).toBe('close');
+  });
+  test('close element is not displayed on load', () => {
+    expect(closeElement.style.display).toBe('none');
+  });
+  test('close element has close icon', () => {
+    const closeIcon = closeElement.querySelector('.close-span');
+    expect(closeIcon).toBeTruthy();
+    expect(closeIcon.textContent).toBe('X');
+  });
+
+  test('close element is displayed when supplied element is clicked', () => {
+    suppliedTestElement.click();
+    expect(closeElement.style.display).toBe('block');
+  });
+  test('close element is hidden on click', () => {
+    closeElement.click();
+    expect(closeElement.style.display).toBe('none');
+  });
 });
 
 describeIf(styleExists)('style tests', () => {
-    test("style element has correct id", () => {
-        expect(styleElement.id).toBe(styleElementID);
-    })
+  test('style element has correct id', () => {
+    expect(styleElement.id).toBe(styleElementID);
+  });
 });
-
-// describe("test setup", () => {
-//
-//
-//
-//     const shouldSkip = () => !shouldRun
-//     const skipIf = () => this.skip()
-//
-//
-//
-//     testIf(shouldRun)('supplied element click event opens gutter', () => {
-//         const suppliedElement = document.getElementById(testElementID);
-//         expect(suppliedElement).toBeTruthy();
-//
-//         suppliedElement.click();
-//
-//         const frame = document.getElementById("bestow-frame");
-//         const gutter = document.getElementById("bestow-gutter");
-//         const closeElem = document.getElementById("bestow-close");
-//
-//         expect(suppliedElement.onclick.name).toEqual('openGutter')
-//     });
-//
-//     testIf(shouldRun)('iframe element exists', () => {
-//         const iframeElem = document.getElementById(iframeElementID);
-//         expect(iframeElem).toBeTruthy();
-//     });
-//
-// });
