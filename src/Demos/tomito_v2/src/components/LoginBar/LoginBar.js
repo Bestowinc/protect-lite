7144d5000f787cb7ef0e082f2d51788c3c6cd794
Josh McDaniel <josh.mcdaniel@sharpnotions.com>
2022-04-21 16:20:11 -0400

    change to naming

--- src/Demos/tomito_v2/src/components/LoginBar/LoginBar.js
+++ src/Demos/tomito_v2/src/components/LoginBar/LoginBar.js
@@ -6,16 +6,16 @@ const LoginBar = ({ setLoggedStatus }) => (
     className="flex justify-end space-y-4 md:space-y-0 md:space-x-4 flex-col md:flex-row"
   >
     <input
-      class="mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"
+      className="mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"
       placeholder="Login ID"
     />
     <input
-      class="mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"
+      className="mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"
       placeholder="Password"
     />
     <Link to="/portfolio">
       <button
-        class="px-6 font-semibold rounded-full bg-[#6082b6] text-white h-8"
+        className="px-6 font-semibold rounded-full bg-[#6082b6] text-white h-8"
         onClick={() => {
           setLoggedStatus(true);
         }}
